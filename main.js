0f701e8a1d202feb6e2015643205b01b2cdd8663
Nanda <nandayavanets@mail.ru>
2024-01-28 21:14:44 +0700

    make propmpt inputted manually

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const rockPaperScissor = ["Rock", "Paper", "Scissor"];
+
 // Function that triggers random output of the Computer's choice
 function game(rockPaperScissor) {
   return rockPaperScissor[Math.floor(Math.random() * rockPaperScissor.length)];
 }
 
+// Function for the outcome after the user inputs either rock, paper, or scissor
 function newGame(playerSelection) {
   function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -35,9 +37,14 @@ function newGame(playerSelection) {
   playRound(playerSelection, computerSelection);
 }
 
-// Add loop to prompt
-const numberOfGames = 3; 
-for (let i = 0; i < numberOfGames; i++) {
+// Function to run the game
+function playNow() {
+  const numberOfGames = 3; 
+  for (let i = 0; i < numberOfGames; i++) {
   let playerStart = prompt(`Game ${i + 1}: Choose your skill: Rock, Paper, Scissor`)
   newGame(playerStart);
 }
+}
+
+// Trigger the function manually after the content is loaded
+window.playNow = playNow;
